7770a517b2b5d925c0124d200b882234334a2e46
chroda <chroda@chroda.com.br>
2020-10-12 19:05:40 +0100

    First lesson from CC: blocks

---
+++ blocks/canvas.js
@@ -0,0 +1,15 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+var c = canvas.getContext("2d");
+
+c.fillRect(100, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
